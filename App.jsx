6d8964f46fc9a5a3e6d4470da1afb327319ecb17
Master Two T <cresson111@gmail.com>
2017-11-05 15:44:36 +0100

    javascript api test

--- App.jsx
+++ App.jsx
@@ -1,55 +1,46 @@
 import React from 'react';
 
 class App extends React.Component {
-   render() {
-      return (
-         <div>
-            <Header/>
-            <Content/>
-            <Apicall/>
-         </div>
-      );
-   }
+  render() {
+    return (<div>
+      <Header/>
+      <Content/>
+      <Apicall/>
+    </div>);
+  }
 }
 
 class Header extends React.Component {
-   render() {
-      return (
-         <div>
-            <h1>Header</h1>
-         </div>
-      );
-   }
+  render() {
+    return (<div>
+      <h1>Header</h1>
+    </div>);
+  }
 }
 
 class Content extends React.Component {
-   render() {
-      return (
-         <div>
-            <h2>Content</h2>
-            <p>The content text!!!</p>
-         </div>
-      );
-   }
+  render() {
+    return (<div>
+      <h2>Content</h2>
+      <p>The content text!!!</p>
+    </div>);
+  }
 }
 
 class Apicall extends React.Component {
   render() {
-    return (
-      <div>
-        <button onClick={() => this.GetCar()}>Get!</button>
-      </div>
-    )
+    return (<div>
+      <button onClick={() => this.GetCar()}>Get!</button>
+    </div>);
   }
 }
 
 function GetCar() {
   var xhttp = new XMLHttpRequest();
-  xhttp.open("GET", "http://localhost:9090/jeyni-service/rest/car", false);
+  xhttp.open("GET", "http://localhost:9090/jeyni-service/rest/car", true);
   xhttp.send();
   var response = JSON.parse(xhttp.responseText);
   alert(response);
-
 }
 
 export default App;
